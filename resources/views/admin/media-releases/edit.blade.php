fd75d760ef3beb919824c521aff24f7db3c484bf
Mark Woodward <markw@digitalbridge.com.au>
2023-03-26 01:46:46 +1100

    edit and index screens

--- resources/views/admin/media-releases/edit.blade.php
+++ resources/views/admin/media-releases/edit.blade.php
@@ -1,36 +1,45 @@
-@extends("admin.layouts.master")
+@extends('admin.layouts.master')
 
 @section('content')
-{!! Form::open(['route' => ['admin.notifications.update', $notification->id], 'method' => 'POST',  'enctype' => 'multipart/form-data']) !!}
+{!! Form::open(['route' => ['admin.media-releases.update', $mediaRelease->id], 'method' => 'POST',  'enctype' => 'multipart/form-data']) !!}
     <div class="section-head">
         <div class="content">
-            <h2 class="mb-2">Edit notification</h2>
+            <h2 class="mb-2">Edit media release</h2>
         </div>
-        {!! Form::submit('Save notification', ['class' => 'button blue flex-shrink-0']) !!}
+        {!! Form::submit('Save media release', ['class' => 'button blue flex-shrink-0']) !!}
     </div>
 
     <div class="card-group mb-5">
         <div class="card-col w-full">
             <div class="card">
-                <div class="flex flex-wrap">
-                    <div class="flex flex-wrap w-full">
-                        <fieldset class="mb-6 px-1 w-full">
-                            {!! Form::label('notification', 'Notification', ['class' => 'field-label']) !!}
-                            {!! Form::text('notification', $notification->notification, ["class" => "field-input"]) !!}
-                            <span class="field-error">{!! $errors->first('notification') !!}
-                        </fieldset>
-                        <fieldset class="mb-6 px-1 w-1/2">
-                            {!! Form::label('display_from', 'Display From', ['class' => 'field-label']) !!}
-                            {!! Form::text('display_from', $notification->display_from ? $notification->display_from : null, ['id' => 'display_from', 'class' => 'field-input flatpickr', 'data-enable-time' => 'true']) !!}
-                            <span class="field-error">{!! $errors->first('display_from') !!}
-                        </fieldset>
-                        <fieldset class="mb-6 px-1 w-1/2">
-                            {!! Form::label('display_to', 'Display To', ['class' => 'field-label']) !!}
-                            {!! Form::text('display_to', $notification->display_to ? $notification->display_to : null, ['id' => 'display_to', 'class' => 'field-input flatpickr', 'data-enable-time' => 'true']) !!}
-                            <span class="field-error">{!! $errors->first('display_to') !!}
-                        </fieldset>
-                    </div>
-                </div>
+                <fieldset class="mb-2 px-1">
+                    {!! Form::label('title', 'Title', ['class' => 'field-label']) !!}
+                    {!! Form::text('title', null, ['class' => 'field-input', 'placeholder' => 'Title']) !!}
+                    <span class="field-error">{!! $errors->first('title') !!}</span>
+                </fieldset>
+                <fieldset class="mb-2s px-1">
+                    {!! Form::label('slug', 'Slug', ['class' => 'field-label']) !!}
+                    {!! Form::text('slug', null, ['class' => 'field-input', 'placeholder' => 'Slug']) !!}
+                    <span class="field-error">{!! $errors->first('slug') !!}</span>
+                </fieldset>
+            </div>
+        </div>
+        <div class="card-col w-full">
+            <div class="card">
+                <fieldset class="mb-2 px-1">
+                    {!! Form::label('subtitle', 'Subtitle', ['class' => 'field-label']) !!}
+                    {!! Form::text('subtitle', null, ['class' => 'field-input', 'placeholder' => 'Subtitle']) !!}
+                    <span class="field-error">{!! $errors->first('subtitle') !!}</span>
+                </fieldset>
+                <fieldset class="mb-2 px-1">
+                    {!! Form::label('content', 'Content', ['class' => 'field-label']) !!}
+                    {!! Form::textarea('content', null, ['class' => 'field-input froala_editor', 'placeholder' => 'Content']) !!}
+                </fieldset>
+
+                <fieldset class="w-full mb-2 px-1">
+                    {!! Form::label('embed', 'Embed Code', ['class' => 'field-label']) !!}
+                    {!! Form::textarea('embed', null, ['class' => 'field-input', 'placeholder' => 'Embed']) !!}
+                </fieldset>
             </div>
         </div>
     </div>
